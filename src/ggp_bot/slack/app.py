13b9be00717f35c55d9f7f67e4d5ffeae4aa3761
Murray Crane <murray.crane@ggpsystems.co.uk>
2026-04-22 10:26:43 +0100

    refactor: register single /ggp command handler
    - Replace 9 individual command registrations with single /ggp handler
    - Import only handle_ggp_command from handlers.commands

--- src/ggp_bot/slack/app.py
+++ src/ggp_bot/slack/app.py
@@ -4,17 +4,7 @@ from slack_bolt.async_app import AsyncApp
 from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
 
 from ggp_bot.config import settings
-from ggp_bot.slack.handlers.commands import (
-    handle_ping_command,
-    handle_intranet_status_command,
-    handle_next_bank_holiday_command,
-    handle_holiday_entitlement_command,
-    handle_my_holidays_command,
-    handle_request_holiday_command,
-    handle_cancel_holiday_command,
-    handle_whoami_command,
-    handle_connect_command,
-)
+from ggp_bot.slack.handlers.commands import handle_ggp_command
 
 
 # Create the Bolt app
@@ -23,16 +13,8 @@ app = AsyncApp(
     signing_secret=settings.slack_signing_secret,
 )
 
-# Register slash command handlers
-app.command("/ggp-ping")(handle_ping_command)
-app.command("/intranet-status")(handle_intranet_status_command)
-app.command("/next-bank-holiday")(handle_next_bank_holiday_command)
-app.command("/holiday-entitlement")(handle_holiday_entitlement_command)
-app.command("/my-holidays")(handle_my_holidays_command)
-app.command("/request-holiday")(handle_request_holiday_command)
-app.command("/cancel-holiday")(handle_cancel_holiday_command)
-app.command("/whoami")(handle_whoami_command)
-app.command("/connect")(handle_connect_command)
+# Register the consolidated slash command handler
+app.command("/ggp")(handle_ggp_command)
 
 
 async def start_app() -> None:
